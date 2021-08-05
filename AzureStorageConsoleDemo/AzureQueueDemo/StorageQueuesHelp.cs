be3ba9a2f62668e6f421e4edebf39265a2fd291a
Wen Yang <v-wenyang@microsoft.com>
2021-08-05 18:29:53 +0800

    add queues

---
+++ AzureStorageConsoleDemo/AzureQueueDemo/StorageQueuesHelp.cs
@@ -0,0 +1,67 @@
+﻿using Azure.Storage.Queues;
+using Azure.Storage.Queues.Models;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AzureStorageConsoleDemo.AzureQueueDemo
+{
+    class StorageQueuesHelp
+    {
+        /// <summary>
+        /// Storage 连接字符串
+        /// </summary>
+        public string StorageConnectionString { get; set; }
+
+        /// <summary>
+        /// 初始化程序配置文件
+        /// </summary>
+        /// <returns>StorageTableHelp</returns>
+        public static StorageQueuesHelp LoadAppSettings()
+        {
+            IConfigurationRoot configRoot = new ConfigurationBuilder().AddJsonFile("Settings.json").Build();
+            StorageQueuesHelp appSettings = configRoot.Get<StorageQueuesHelp>();
+            return appSettings;
+        }
+
+        /// <summary>
+        /// 创建一个 Queues
+        /// </summary>
+        /// <param name="queuesName">Queues 的名称</param>
+        /// <returns>QueueClient</returns>
+        public static async Task<QueueClient> CreateQueueClientAsync(string queuesName)
+        {
+            string connectionString = LoadAppSettings().StorageConnectionString;
+            QueueClient queueClient = new QueueClient(connectionString, queuesName);
+            await queueClient.CreateAsync();
+            return queueClient;
+        }
+
+        /// <summary>
+        /// 向队列中插入一条消息 默认永不过期
+        /// </summary>
+        /// <param name="theQueue">QueueClient</param>
+        /// <param name="newMessage">消息字符串</param>
+        /// <returns></returns>
+        public static async Task<SendReceipt> InsertMessageAsync(QueueClient theQueue, string newMessage, bool isInvalid = false)
+        {
+            // 检测队列是否创建，如果没有创建则创建，否则不做操作
+            await theQueue.CreateIfNotExistsAsync();
+            // 向队列发送一条新的消息
+            SendReceipt sendReceipt;
+            if (isInvalid)
+            {
+                // 默认生存周期为 7 天
+                sendReceipt = await theQueue.SendMessageAsync(newMessage);
+            }
+            else
+            {
+                // 永不过期消息
+                sendReceipt = await theQueue.SendMessageAsync(newMessage, default, TimeSpan.FromSeconds(-1), default);
+            }
+            return sendReceipt;
+        }
+    }
+}
--- AzureStorageConsoleDemo/Program.cs
+++ AzureStorageConsoleDemo/Program.cs
@@ -1,5 +1,9 @@
-﻿using AzureStorageConsoleDemo.AzureTableDemo;
+﻿using Azure.Storage.Queues;
+using Azure.Storage.Queues.Models;
+using AzureStorageConsoleDemo.AzureQueueDemo;
+using AzureStorageConsoleDemo.AzureTableDemo;
 using Microsoft.Azure.Cosmos.Table;
+using Microsoft.Extensions.Configuration;
 using System;
 using System.Collections.Generic;
 using System.Data;
@@ -11,7 +15,36 @@ namespace AzureStorageConsoleDemo
     {
         static async Task Main(string[] args)
         {
+            //string queueName = "queues" + Guid.NewGuid().ToString().Substring(0, 5);
+            string queueName = "queues68f86";
+            QueueClient queueClient = await StorageQueuesHelp.CreateQueueClientAsync(queueName);
+            //Console.WriteLine("\n正在更新队列中的第三条消息...");
 
+            //// 使用发送消息时保存的回执更新消息
+            //await queueClient.UpdateMessageAsync(receipt.MessageId, receipt.PopReceipt, "第三条消息已更新");
+
+            //Console.WriteLine("\n正在接收来自队列的消息...");
+
+            // 从队列中获取消息
+            QueueMessage[] messages = await queueClient.ReceiveMessagesAsync(maxMessages: 10);
+            Console.WriteLine("\n按 Enter 键“处理”消息并将它们从队列中删除...");
+            Console.ReadLine();
+
+            //// 处理和删除队列中的消息
+            //foreach (QueueMessage message in messages)
+            //{
+            //    // “处理”消息
+            //    Console.WriteLine($"消息： {message.MessageText}");
+
+            //    // 让服务知道我们已处理完该消息，并且可以安全地将其删除。
+            //    await queueClient.DeleteMessageAsync(message.MessageId, message.PopReceipt);
+            //}
+            //Console.WriteLine("\n按 Enter 键删除队列...");
+            //Console.ReadLine();
+
+            //// 清理
+            //Console.WriteLine($"删除队列： {queueClient.Name}");
+            //await queueClient.DeleteAsync();
             Console.ReadLine();
         }
     }
